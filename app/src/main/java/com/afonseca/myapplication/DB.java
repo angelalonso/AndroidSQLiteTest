72e42258df742e000fe0906b33979f2d8b661dcf
unknown <afonseca@BERAFONSECA-L1.jcorp.ad.jamba.net>
2015-01-23 07:29:08 +0100

    Initial commit

---
+++ app/src/main/java/com/afonseca/myapplication/DB.java
@@ -0,0 +1,13 @@
+package com.afonseca.myapplication;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by afonseca on 1/23/2015.
+ */
+public class DB  {
+
+    protected void onCreate() {
+
+    }
+}
